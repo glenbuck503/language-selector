f0c492735ca4f3c53b2eda371c995a339873ba61
glenbuck503 <glenbuck503@gmail.com>
2020-10-23 11:26:48 -0700

    add jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
 $(document).ready(function() {
   $("form#questions").submit(function(event){
-    const quest1String = $("select#quest1").val();
-    const quest1 = parseInt(quest1String);
-    const quest2String = $("select#quest2").val();
-    const quest2 = parseInt(quest2String);
-    const quest3String = $("select#quest3").val();
-    const quest3 = parseInt(quest3String);
-    const quest4String = $("select#quest4").val();
-    const quest4 = parseInt(quest4String);
-    const quest5String = $("select#quest5").val();
-    const quest5 = parseInt(quest5String);
+    event.preventDefault();
 
+const quest1String = $("select#quest1").val();
+const quest3 = parseInt(quest3String);
+const quest2String = $("select#quest2").val();
+const quest3 = parseInt(quest3String);
+const quest3String = $("select#quest3").val();
+const quest3 = parseInt(quest3String);
+const quest4String = $("select#quest4").val();
+const quest4 = parseInt(quest4String);
+const quest5String = $("select#quest5").val();
+const quest5 = parseInt(quest5String);
+
+const result = quest5 + quest4;
+
+if (result >= 7) {
+  alert("dfsd");
+} else if( result < 7){
+  alert("dsfsd");
+}
 
 
   });
 });
+
+
