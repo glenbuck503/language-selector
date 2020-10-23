4f99a0aa9fb7ec8f21bd9273f2f6ef7d6de1b702
glenbuck503 <glenbuck503@gmail.com>
2020-10-23 11:35:47 -0700

    change approach to jquery

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,14 @@ $(document).ready(function() {
   $("form#questions").submit(function(event){
     event.preventDefault();
 
-const quest1String = $("select#quest1").val();
-const quest3 = parseInt(quest3String);
-const quest2String = $("select#quest2").val();
-const quest3 = parseInt(quest3String);
-const quest3String = $("select#quest3").val();
-const quest3 = parseInt(quest3String);
-const quest4String = $("select#quest4").val();
-const quest4 = parseInt(quest4String);
-const quest5String = $("select#quest5").val();
-const quest5 = parseInt(quest5String);
+    const frontEnd = $("select#quest1").val();
+    const start = $("select#quest2").val();
 
-const result = quest5 + quest4;
+    if (frontEnd === "yes" || start === "twelve") {
+      alert("dsfsd");
+    }
 
-if (result >= 7) {
-  alert("dfsd");
-} else if( result < 7){
-  alert("dsfsd");
-}
+
 
 
   });
